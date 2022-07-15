3c14c64ec988f9da3bbbddb54947fdaef011e036
rajpranit <rajpranit1207@gmail.com>
2022-07-15 09:11:11 +0530

    Fixing Data Lost Because of Stateless widget (bottom sheet)

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/material.dart';
 
-class NewTransactions extends StatelessWidget {
+class NewTransactions extends StatefulWidget {
   final Function newTansactions;
-  final textInput = TextEditingController();
-  final amountInput = TextEditingController();
 
   NewTransactions(this.newTansactions);
 
-  void onSubmitted()  {
+  @override
+  State<NewTransactions> createState() => _NewTransactionsState();
+}
 
+class _NewTransactionsState extends State<NewTransactions> {
+  final textInput = TextEditingController();
+
+  final amountInput = TextEditingController();
+
+  void _onSubmitted() {
     final enteredTitle = textInput.text;
     final enteredAmount = double.parse(amountInput.text);
 
-    if(enteredTitle == null || enteredAmount <=0 ){
+    if (enteredTitle == null || enteredAmount <= 0) {
       return;
     }
-        newTansactions(
-          enteredTitle,
-          enteredAmount
-        );
-}
+    widget.newTansactions(enteredTitle, enteredAmount);
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 15,
       child: Container(
-
         padding: EdgeInsets.all(10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
@@ -36,7 +40,7 @@ class NewTransactions extends StatelessWidget {
               cursorColor: Color(0xFF2F3C7E),
               textAlign: TextAlign.center,
               autofocus: false,
-              onSubmitted: (_) => onSubmitted(),
+              onSubmitted: (_) => _onSubmitted(),
               decoration: InputDecoration(
                   labelText: 'Item', hoverColor: Color(0xFF2F3C7E)),
             ),
@@ -45,11 +49,11 @@ class NewTransactions extends StatelessWidget {
               controller: amountInput,
               textAlign: TextAlign.center,
               autofocus: false,
-              onSubmitted: (_) => onSubmitted(),
+              onSubmitted: (_) => _onSubmitted(),
               decoration: InputDecoration(labelText: 'Price'),
             ),
             TextButton(
-              onPressed: onSubmitted,
+              onPressed: _onSubmitted,
               child: Text('Add Transaction'),
               style: TextButton.styleFrom(
                 primary: Color(0xFF317773),
